3864ccff6af01874414aae6e467040322f20ef6b
nifle3 <nifle3123@gmail.com>
2026-07-21 19:45:19 +0300

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -2,20 +2,18 @@
   description = "A development shell with Ansible, Git, and Python 3";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
 
   outputs = { self, nixpkgs }: {
     devShells.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.mkShell {
       packages = with nixpkgs.legacyPackages.x86_64-linux; [
         ansible
-        git
-        python3
       ];
 
       shellHook = ''
-        echo "Добро пожаловать в devShell с Ansible, Git и Python 3!"
-        echo "Используйте 'ansible --version', 'git --version' и 'python3 --version' для проверки."
+        echo "Добро пожаловать в devShell с Ansible"
+        echo "Используйте 'ansible --version'"
       '';
     };
   };
